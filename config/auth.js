1a90dae5de1bbac6f10d09a3aebe63b3c4f8376e
elewho <32118433+imnotdanii@users.noreply.github.com>
2022-09-22 23:11:27 +1000

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -1,9 +1,9 @@
 module.exports = {
-    ensureAuthenticated: function(req, res, next){
+    ensureAuthenticated: function(req, res, next){ //Another passport feature, checks if user is logged in/authenticated
         if(req.isAuthenticated()){
             return next();
         }
-        req.flash('error_msg', 'Not logged in');
+        req.flash('error_msg', 'Please log in to view this page.');
         res.redirect('/users/login');
     }
 }
