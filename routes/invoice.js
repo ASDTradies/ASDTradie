68c270827bbcb64bf6e7b44100ca5c71e99b8960
Myzna1 <zacharymeissner1@gmail.com>
2022-10-19 16:13:13 +1100

    hash

--- routes/invoice.js
+++ routes/invoice.js
@@ -1,47 +1,13 @@
 let express = require('express');
 let router = express.Router();
 let uniqid = require('uniqid');
-const bcrypt = require('bcryptjs'); //Bcrypt - for lightweight hashing
+const crypto = require('crypto'); 
+let salt = 'f844b09ff50c'
 const Invoice = require('../models/invoice-model').Invoice;
-
-// router.get('/invoiceGen', async (req, res)=> {
-//     let invoice = Invoice.findOne({first_name: res.newInvoice.first_name});
-//     res.render('invoiceGen.ejs', invoice);
-// })
 
 router.get('/billingForm', (req, res)=> {
     res.render('billingForm.ejs')
 })
-
-//router.get('/index', (req, res)=> {
-//    res.render('index.html')
-//})
-
-/*router.get('/orderHistory', (req, res)=> {
-    Invoice.find({}).then((invoice) =>{
-        res.send(invoice);
-        res.render('orderHistory.ejs')
-    }).catch((error) => {
-        res.status(500).send(error);
-    })
-})
-*/
-
-/*const invoiceSchema = {
-    //   invId: String,
-    //   invRequestID: String,
-       service_title: String,
-       userID: String,
-       tradieID: String,
-       street_address: String,
-       date: String,
-       price: String,
-         //first_name: String,
-       street_address: String
-   }
-
-   const Invoice = mongoose.model('Invoice', invoiceSchema);
-   */
 
 router.get('/orderHistory', (req, res)=> {
     //var query = {userID: "112"};
@@ -49,7 +15,6 @@ router.get('/orderHistory', (req, res)=> {
         res.render('orderHistory', {
             invoiceList: invoices
         })
-        
     })
 })
 
@@ -57,11 +22,8 @@ router.get('/workHistory', (req, res)=> {
     Invoice.find({}, function(err, invoices){
         res.render('workHistory', {
             invoiceList: invoices
-        })
-       
+        })  
     })
-        
-
 })
 
 router.post('search', async (req, res)=>{
@@ -72,7 +34,6 @@ router.post('search', async (req, res)=>{
         })
     })
 })
-
 
 router.post('/billingForm',  async (req, res) =>{
     //const { invRequestID, invServiceID, invCustomerID, invTradieID, invAddress, invDate, invPrice, invPaid} = req.body;
@@ -94,90 +55,11 @@ router.post('/billingForm',  async (req, res) =>{
         price 
         //invPaid
     });
-    bcrypt.genSalt(10, function(err, salt){ //Hashing the password before storage
-        bcrypt.hash(newInvoice.userID, salt, (err,hash) => {
-            if(err) throw err;
-            newInvoice.userID = hash;
-        })
-    })
-    bcrypt.genSalt(10, function(err, salt){ //Hashing the password before storage
-        bcrypt.hash(newInvoice.first_name, salt, (err,hash) => {
-            if(err) throw err;
-            newInvoice.first_name = hash;
-        })
-    })
-    bcrypt.genSalt(10, function(err, salt){ //Hashing the password before storage
-        bcrypt.hash(newInvoice.service_requestID, salt, (err,hash) => {
-            if(err) throw err;
-            newInvoice.service_requestID = hash;
-        })
-    })
-    bcrypt.genSalt(10, function(err, salt){ //Hashing the password before storage
-        bcrypt.hash(newInvoice.service_title, salt, (err,hash) => {
-            if(err) throw err;
-            newInvoice.service_title = hash;
-        })
-    })
-    bcrypt.genSalt(10, function(err, salt){ //Hashing the password before storage
-        bcrypt.hash(newInvoice.last_name, salt, (err,hash) => {
-            if(err) throw err;
-            newInvoice.last_name = hash;
-        })
-    })
-    bcrypt.genSalt(10, function(err, salt){ //Hashing the password before storage
-        bcrypt.hash(newInvoice.tradieID, salt, (err,hash) => {
-            if(err) throw err;
-            newInvoice.tradieID = hash;
-        })
-    })
-    bcrypt.genSalt(10, function(err, salt){ //Hashing the password before storage
-        bcrypt.hash(newInvoice.hours, salt, (err,hash) => {
-            if(err) throw err;
-            newInvoice.hours = hash;
-        })
-    })
-    bcrypt.genSalt(10, function(err, salt){ //Hashing the password before storage
-        bcrypt.hash(newInvoice.street_address, salt, (err,hash) => {
-            if(err) throw err;
-            newInvoice.street_address = hash;
-        })
-    })
-    bcrypt.genSalt(10, function(err, salt){ //Hashing the password before storage
-        bcrypt.hash(newInvoice.date, salt, (err,hash) => {
-            if(err) throw err;
-            newInvoice.date = hash;
-        })
-    })
-    bcrypt.genSalt(10, function(err, salt){ //Hashing the password before storage
-        bcrypt.hash(newInvoice.phone_number, salt, (err,hash) => {
-            if(err) throw err;
-            newInvoice.phone_number = hash;
-        })
-    })
-    bcrypt.genSalt(10, function(err, salt){ //Hashing the password before storage
-        bcrypt.hash(newInvoice.city, salt, (err,hash) => {
-            if(err) throw err;
-            newInvoice.city = hash;
-        })
-    })
-    bcrypt.genSalt(10, function(err, salt){ //Hashing the password before storage
-        bcrypt.hash(newInvoice.state, salt, (err,hash) => {
-            if(err) throw err;
-            newInvoice.state = hash;
-        })
-    })
-    bcrypt.genSalt(10, function(err, salt){ //Hashing the password before storage
-        bcrypt.hash(newInvoice.post_code, salt, (err,hash) => {
-            if(err) throw err;
-            newInvoice.post_code = hash;
-        })
-    })
-    bcrypt.genSalt(10, function(err, salt){ //Hashing the password before storage
-        bcrypt.hash(newInvoice.price, salt, (err,hash) => {
-            if(err) throw err;
-            newInvoice.price = hash;
-        })
-    })
+    Object.values(newInvoice).forEach(val =>{
+        let hash = crypto.pbkdf2Sync(newInvoice.val, salt, 1000, 64, 'sha512').toString('hex');
+        newInvoice.val = hash;
+    });
+
     newInvoice.save()
     .then(newInvoice =>{
 // console.log("newinvoice" + newInvoice)
