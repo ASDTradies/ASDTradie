c140389ab2a87cd7c0102352030a2487e78f2771
Myzna1 <zacharymeissner1@gmail.com>
2022-10-21 14:16:59 +1100

    stuff

--- routes/invoice.js
+++ routes/invoice.js
@@ -7,11 +7,6 @@ let Service = require('../models/services-model').Service;
 const { forwardAuthenticated, ensureAuthenticated } = require('../config/auth');
 
 
-// router.get('/invoiceGen', async (req, res)=> {
-//     let invoice = Invoice.findOne({first_name: res.newInvoice.first_name});
-//     res.render('invoiceGen.ejs', invoice);
-// })
-
 router.get('/billingForm', ensureAuthenticated, (req, res)=> {
 //Get serviceRequest where service request customer id = current session id
     ServiceRequest.findOne({customerID : req.session.userID}).then(serviceRequest => { 
@@ -24,35 +19,6 @@ router.get('/billingForm', ensureAuthenticated, (req, res)=> {
     .catch(err => console.log(err))
 })
 
-//router.get('/index', (req, res)=> {
-//    res.render('index.html')
-//})
-
-/*router.get('/orderHistory', (req, res)=> {
-    Invoice.find({}).then((invoice) =>{
-        res.send(invoice);
-        res.render('orderHistory.ejs')
-    }).catch((error) => {
-        res.status(500).send(error);
-    })
-})
-*/
-
-/*const invoiceSchema = {
-    //   invId: String,
-    //   invRequestID: String,
-       service_title: String,
-       userID: String,
-       tradieID: String,
-       street_address: String,
-       date: String,
-       price: String,
-         //first_name: String,
-       street_address: String
-   }
-
-   const Invoice = mongoose.model('Invoice', invoiceSchema);
-   */
 
 router.get('/orderHistory', ensureAuthenticated, (req, res)=> {
     Invoice.find({}, function(err, invoices){
@@ -98,13 +64,11 @@ router.post('/billingForm',  ensureAuthenticated, async (req, res) =>{
         state, 
         post_code,
         price 
-        //invPaid
     });
 
     
     await newInvoice.save()
     .then(newInvoice =>{
-        // console.log("newinvoice" + newInvoice)
         res.render('invoiceGen.ejs', {invoice: newInvoice});
     })
 
