84241b41ea1007a1d2f66f12c2ff8f3cd5c0c3d4
Myzna1 <zacharymeissner1@gmail.com>
2022-10-23 23:28:26 +1100

    error messages

--- routes/invoice.js
+++ routes/invoice.js
@@ -10,13 +10,13 @@ const { forwardAuthenticated, ensureAuthenticated } = require('../config/auth');
 router.get('/billingForm', ensureAuthenticated, (req, res)=> {
 //Get serviceRequest where service request customer id = current session id
     
-    ServiceRequest.findOne({customerID : req.session.userID}).then(serviceRequest => { 
-    let service = Service.findOne({id: ServiceRequest.serviceID});
-        console.log(service.serviceTitle)
-        console.log(serviceRequest)
+    ServiceRequest.findOne({customerID : req.session.userID}).then(async serviceRequest => { 
+        //let service = await Service.findOne({id: ServiceRequest.serviceID});
+        //console.log(serviceRequest.service.serviceTitle)
+        //console.log(serviceRequest)
         if(serviceRequest){
             
-            res.render('billingForm.ejs', {serviceRequest, serviceTitle: service.serviceTitle});
+            res.render('billingForm.ejs', {serviceRequest/*, serviceTitle: service.serviceTitle*/});
         }
         
     })
@@ -69,13 +69,28 @@ router.post('/billingForm',  ensureAuthenticated, async (req, res) =>{
     //const { invRequestID, invServiceID, invCustomerID, invTradieID, invAddress, invDate, invPrice, invPaid} = req.body;
     const { service_requestID, service_title , userID , first_name , last_name , tradieID , hours , street_address , phone_number, city, state, post_code, price, cardNumber, cardName, expiry, verification } = req.body;
     var errors = []; //Error messages
-
     //Check required fields
     if(!first_name || !last_name || !street_address || !phone_number || !city || !state || !post_code|| !cardNumber|| !cardName || !expiry || !verification){
-        errors.push({msg: 'Please fill in all fields'});
+        errors.push({msg: 'Please fill in all "Non Optional" fields'});
     }
-    if(phone_number.length != 10 || post_code.length != 4 || expiry.length != 4 || cardNumber.length != 16 || verification.length != 3){
-        errors.push({msg: 'Please fill fields to correct length'});
+    if(parseInt(phone_number).length != 10){
+        errors.push({msg: 'Please fill "Phone Number" to correct length and type'});
+    }
+    if(parseInt(post_code).length != 4){
+        errors.push({msg: 'Please fill "Postcode" to correct length and type'});
+    }
+    if(parseInt(expiry).length != 4){
+        errors.push({msg: 'Please fill "Card Expiry Date" to correct length and type'});
+    }
+    if(parseInt(cardNumber).length != 16){
+        errors.push({msg: 'Please "Card Numbers" to correct length and type'});
+    }
+    if(parseInt(verification).length != 3){
+        errors.push({msg: 'Please fill "Verification Numbers" to correct length and type'});
+    }
+    if(errors.length > 0){    //If there is an error, render the error messages
+        req.flash('error', errors[0].msg)
+        res.redirect('/invoice/billingForm')
     }
     else{
     const newInvoice = new Invoice({
